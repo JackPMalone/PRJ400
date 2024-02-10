8dc1ca625e94d6ceb43dc38606df0c0c41cffc99
S00199917 <S00199917@mail.itsligo.ie>
2024-02-10 23:17:04 +0000

    Added Main/Base component
    This is viewed when a user has logged in
    
    Currently no login feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './main/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
