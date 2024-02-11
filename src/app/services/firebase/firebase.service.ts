9c104d2938865e04a8fc14fa72237be41a022d3d
S00199917 <S00199917@mail.itsligo.ie>
2024-02-11 15:06:15 +0000

    Updated firebase service
    The service is now able to interact with the firebase functions

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  private baseUrl = 'https://us-central1-prj400-c5407.cloudfunctions.net'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getHelloWorld(): Observable<string>{
+    return this.http.get<string>(`${this.baseUrl}/helloWorld`)
+  }
 }
