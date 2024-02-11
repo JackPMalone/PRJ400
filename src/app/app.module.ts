196aa938c1502b336e47a3c1a2020557f3c2e5d7
S00199917 <S00199917@mail.itsligo.ie>
2024-02-11 00:04:38 +0000

    App-Main-Base has tempory values
    The app now displays some default values, currently there are errors as App-Main-Device needs to be updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BaseComponent } from './main/base/base.component';
 import { DeviceComponent } from './main/device/device.component';
 
+import { NgChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { DeviceComponent } from './main/device/device.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
