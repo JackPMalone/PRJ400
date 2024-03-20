cefa5c7e52b30fac0a8cce3e6b1ee0b3725e8825
S00199917 <S00199917@mail.itsligo.ie>
2024-03-20 11:45:27 +0000

    Added the new device page
    The page will be used to connect the account with the device

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 //Components
 import { BaseComponent as MainBase } from './main/base/base.component';
 import { BaseComponent as AuthBase } from './auth/base/base.component';
+import { BaseComponent as DeviceBase } from './newDevice/base/base.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: MainBase },
-  { path: 'login', component: AuthBase }
+  { path: 'login', component: AuthBase },
+  { path: 'new', component: DeviceBase}
 ];
 
 @NgModule({
